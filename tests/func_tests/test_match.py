166d3f57fbd8ced065374244afdb9f796cabd70a
madscout12 <zkoopmans@gmail.com>
2018-01-19 10:41:32 -0600

    Adding Functional Test for Match

--- tests/func_tests/test_match.py
+++ tests/func_tests/test_match.py
@@ -2,16 +2,32 @@ import sys
 import unittest
 import os
 import shutil
+import subprocess
 
-sys.path.append("../../src")
-
-from main import organize_data
-
+script = "python3 src/main.py match -k first_name -f tests/test_data/match_data.json -o match_test"
 
 class TestMatch(unittest.TestCase):
 
+    def setUp(self):
+        os.chdir("../..")
+        try:
+            os.mkdir("match_test")
+        except OSError:
+            pass
+
+    def tearDown(self):
+        shutil.rmtree("match_test")
+
     def test_match(self):
-        pass
+        print(os.getcwd())
+        proc = subprocess.Popen(script, shell=True)
+        proc.wait()
+        self.assertEqual(0, proc.returncode, msg=proc.stderr)
+
+
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
