20b4a080e91ef0c67f05d48643cef35b596f4327
madscout12 <zkoopmans@gmail.com>
2018-01-19 09:03:08 -0600

    Getting tests to pass on CircleCI

---
+++ tests/func_tests/test_match.py
@@ -0,0 +1,17 @@
+import sys
+import unittest
+import os
+import shutil
+
+sys.path.append("../../src")
+
+from main import organize_data
+
+
+class TestMatch(unittest.TestCase):
+
+    def test_match(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
